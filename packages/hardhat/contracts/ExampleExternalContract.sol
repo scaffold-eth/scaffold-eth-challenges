11ff88feebf3a0d5811b1afa7a61985b4f08a650
Jason Romero <codenamejason@outlook.com>
2021-11-20 16:20:55 -0500

    update open zeppelin to v4

--- packages/hardhat/contracts/ExampleExternalContract.sol
+++ packages/hardhat/contracts/ExampleExternalContract.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.6.0 <0.7.0;
+pragma solidity 0.8.4;
 
 contract ExampleExternalContract {
 
--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.6.0 <0.7.0;
+pragma solidity 0.8.4;
 
 import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
