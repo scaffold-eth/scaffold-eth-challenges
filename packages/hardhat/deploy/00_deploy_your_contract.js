4f1364200a2689b5682b13b80a9d9fa10315e200
Jason Romero <codenamejason@outlook.com>
2021-11-20 16:19:09 -0500

    update open zeppelin to v4

--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -17,37 +17,24 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   // Getting a previously deployed contract
   const yourCollectible = await ethers.getContract("YourCollectible", deployer);
 
-  /*  await YourContract.setPurpose("Hello");
-
-    To take ownership of yourContract using the ownable library uncomment next line and add the
-    address you want to be the owner.
-    // yourContract.transferOwnership(YOUR_ADDRESS_HERE);
-
-    //const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
-  */
-
-  /*
-  //If you want to send value to an address from the deployer
-  const deployerWallet = ethers.provider.getSigner()
-  await deployerWallet.sendTransaction({
-    to: "0x34aA3F359A9D614239015126635CE7732c18fDF3",
-    value: ethers.utils.parseEther("0.001")
-  })
-  */
-
-  /*
-  //If you want to send some ETH to a contract on deploy (make your constructor payable!)
-  const yourContract = await deploy("YourContract", [], {
-  value: ethers.utils.parseEther("0.05")
-  });
-  */
-
-  /*
-  //If you want to link a library into your contract:
-  // reference: https://github.com/austintgriffith/scaffold-eth/blob/using-libraries-example/packages/hardhat/scripts/deploy.js#L19
-  const yourContract = await deploy("YourContract", [], {}, {
-   LibraryName: **LibraryAddress**
-  });
-  */
+  // ToDo: Verify your contract with Etherscan for public chains
+  // if (chainId !== "31337") {
+  //   try {
+  //     console.log(" 🎫 Verifing Contract on Etherscan... ");
+  //     await sleep(5000); // wait 5 seconds for deployment to propagate
+  //     await run("verify:verify", {
+  //       address: yourCollectible.address,
+  //       contract: "contracts/YourCollectible.sol:YourCollectible",
+  //       // contractArguments: [yourToken.address],
+  //     });
+  //   } catch (e) {
+  //     console.log(" ⚠️ Failed to verify contract on Etherscan ");
+  //   }
+  // }
 };
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 module.exports.tags = ["YourCollectible"];
