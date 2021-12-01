23cc9e838657ee253a160aaa5552325c4c20ad76
Jason Romero <codenamejason@outlook.com>
2021-12-01 16:07:10 -0500

    rename deploy file

--- packages/hardhat/deploy/00_deploy_balloons_dex.js
+++ packages/hardhat/deploy/00_deploy_balloons_dex.js
@@ -1,4 +1,4 @@
-// deploy/00_deploy_your_contract.js
+// deploy/00_deploy_balloons_dex.js
 
 const { ethers } = require("hardhat");
 
@@ -6,16 +6,13 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
   await deploy("Balloons", {
-    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
     from: deployer,
-    // args: [ "Hello", ethers.utils.parseEther("1.5") ],
     log: true,
   });
 
   const balloons = await ethers.getContract("Balloons", deployer);
 
   await deploy("DEX", {
-    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
     from: deployer,
     args: [balloons.address],
     log: true,
@@ -30,7 +27,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   // console.log("Approving DEX ("+dex.address+") to take Balloons from main account...")
   // If you are going to the testnet make sure your deployer account has enough ETH
   // await balloons.approve(dex.address,ethers.utils.parseEther('100'));
-  // // console.log("INIT exchange...")
+  // console.log("INIT exchange...")
   // await dex.init(""+(3*10**18),{value:ethers.utils.parseEther('3'),gasLimit:200000})
 };
 module.exports.tags = ["Balloons", "DEX"];
