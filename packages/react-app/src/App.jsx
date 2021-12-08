99b8a7886f5bd58eec4d953be8beaa08ab936679
Jason Romero <codenamejason@outlook.com>
2021-12-08 15:02:11 -0500

    latest

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -598,6 +598,46 @@ function App(props) {
         },
       ],
     },
+    5: {
+      description: "So delicate.",
+      external_url: "https://austingriffith.com/portfolio/paintings/", // <-- this can link to a page for the specific file too
+      image: "https://austingriffith.com/images/paintings/flamingo.jpg",
+      name: "Flamingo",
+      attributes: [
+        {
+          trait_type: "BackgroundColor",
+          value: "black",
+        },
+        {
+          trait_type: "Eyes",
+          value: "googly",
+        },
+        {
+          trait_type: "Stamina",
+          value: 6,
+        },
+      ],
+    },
+    6: {
+      description: "Raaaar!",
+      external_url: "https://austingriffith.com/portfolio/paintings/", // <-- this can link to a page for the specific file too
+      image: "https://austingriffith.com/images/paintings/godzilla.jpg",
+      name: "Godzilla",
+      attributes: [
+        {
+          trait_type: "BackgroundColor",
+          value: "orange",
+        },
+        {
+          trait_type: "Eyes",
+          value: "googly",
+        },
+        {
+          trait_type: "Stamina",
+          value: 99,
+        },
+      ],
+    },
   };
 
   const mintItem = async () => {
