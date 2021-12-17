56afa983589da1dccdb5775e6dc7f878fda7c3d1
ZakGriffith <zakgriffith@gmail.com>
2021-12-16 21:15:28 -0700

    App.jsx edit
    Cleaned up commented out line that was appearing on the frontend.

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -625,8 +625,7 @@ function App(props) {
               </Card>
             </div>
 
-            /*
-            //Extra UI for buying the tokens back from the user using "approve" and "sellTokens"
+            {/*Extra UI for buying the tokens back from the user using "approve" and "sellTokens"
             <Divider />
             <div style={{ padding: 8, marginTop: 32, width: 300, margin: "auto" }}>
               <Card title="Sell Tokens">
@@ -678,7 +677,7 @@ function App(props) {
               </Card>
             </div>
 
-            */
+            */}
 
             <div style={{ padding: 8, marginTop: 32 }}>
               <div>Vendor Token Balance:</div>
