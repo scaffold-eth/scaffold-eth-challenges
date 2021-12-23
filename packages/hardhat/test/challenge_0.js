4dd17a2825118e9c4b4afb03a582647dad7fec69
Austin Griffith <you@example.com>
2021-12-23 10:14:35 -0700

    prepare testing for remote server

--- packages/hardhat/test/challenge_0.js
+++ packages/hardhat/test/challenge_0.js
@@ -1,4 +1,15 @@
-const { ethers } = require("hardhat");
+//
+// this script executes when you run 'yarn test'
+//
+// you can also test remote submissions like:
+// CONTRACT_ADDRESS=0x43Ab1FCd430C1f20270C2470f857f7a006117bbb yarn test --network rinkeby
+//
+// you can even run mint commands if the tests pass like:
+// yarn test && echo "PASSED" || echo "FAILED"
+//
+
+const hre = require("hardhat");
+const { ethers } = hre;
 const { use, expect } = require("chai");
 const { solidity } = require("ethereum-waffle");
 
@@ -7,26 +18,46 @@ use(solidity);
 describe("🚩 Challenge 0: 🎟 Simple NFT Example 🤓", function () {
   let myContract;
 
-  describe("YourCollectible", function () {
-    it("Should deploy YourCollectible", async function () {
-      const YourCollectible = await ethers.getContractFactory("YourCollectible");
+  //console.log("hre:",Object.keys(hre)) // <-- you can access the hardhat runtime env here
 
-      myContract = await YourCollectible.deploy();
-    });
+  describe("YourCollectible", function () {
+
+    if(process.env.CONTRACT_ADDRESS){
+      it("Should connect to external contract", async function () {
+        myContract = await ethers.getContractAt("YourCollectible",process.env.CONTRACT_ADDRESS);
+        console.log("     🛰 Connected to external contract",myContract.address)
+      });
+    }else{
+      it("Should deploy YourCollectible", async function () {
+        const YourCollectible = await ethers.getContractFactory("YourCollectible");
+        myContract = await YourCollectible.deploy();
+      });
+    }
 
     describe("mintItem()", function () {
       it("Should be able to mint an NFT", async function () {
-        const [owner] = await ethers.getSigners();
+        const [ owner ] = await ethers.getSigners();
+
+        console.log('\t'," 🧑‍🏫 Tester Address: ",owner.address)
+
         const startingBalance = await myContract.balanceOf(owner.address)
-        await myContract.mintItem(owner.address, "QmfVMAmNM1kDEBYrC2TPzQDoCRFH6F5tE1e9Mr4FkkR5Xr");
+        console.log('\t'," ⚖️ Starting balance: ",startingBalance.toNumber())
+
+        console.log('\t'," 🔨 Minting...")
+        const mintResult = await myContract.mintItem(owner.address, "QmfVMAmNM1kDEBYrC2TPzQDoCRFH6F5tE1e9Mr4FkkR5Xr");
+        console.log('\t'," 🏷  mint tx: ",mintResult.hash)
+
+        console.log('\t'," ⏳ Waiting for confirmation...")
+        const txResult =  await mintResult.wait()
+        expect(txResult.status).to.equal(2);
+
+        console.log('\t'," 🔎 Checking new balance: ",startingBalance.toNumber())
         expect(await myContract.balanceOf(owner.address)).to.equal(startingBalance.add(1));
 
-        const token = await myContract.tokenOfOwnerByIndex(owner.address,startingBalance);
-        expect(token.toNumber()).to.greaterThan(0);
       });
 
       it("Should track tokens of owner by index", async function () {
-        const [owner] = await ethers.getSigners();
+        const [ owner ] = await ethers.getSigners();
         const startingBalance = await myContract.balanceOf(owner.address)
         const token = await myContract.tokenOfOwnerByIndex(owner.address,startingBalance.sub(1));
         expect(token.toNumber()).to.greaterThan(0);
