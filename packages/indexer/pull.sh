c7ee730a9101de81e6738923c7e4b8b0efdd4346
Austin Griffith <austingriffith@Austins-MacBook-Pro.local>
2021-12-29 10:16:40 -0700

    more commands

---
+++ packages/indexer/pull.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CMD="cd challenge-X-transaction-script && git pull"
+
+ssh beast1 "$CMD"
+ssh beast2 "$CMD"
+ssh beast3 "$CMD"
+ssh beast4 "$CMD"
+ssh beast5 "$CMD"
+ssh beast6 "$CMD"
+ssh beast7 "$CMD"
+ssh beast8 "$CMD"
+ssh beast9 "$CMD"
+ssh beast10 "$CMD"
+ssh beast11 "$CMD"
+ssh beast12 "$CMD"
---
+++ packages/indexer/status.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CMD="cd challenge-X-transaction-script/packages/indexer && node blocknumber && ls blocks/* | wc -l"
+
+ssh beast1 "$CMD"
+ssh beast2 "$CMD"
+ssh beast3 "$CMD"
+ssh beast4 "$CMD"
+ssh beast5 "$CMD"
+ssh beast6 "$CMD"
+ssh beast7 "$CMD"
+ssh beast8 "$CMD"
+ssh beast9 "$CMD"
+ssh beast10 "$CMD"
+ssh beast11 "$CMD"
+ssh beast12 "$CMD"
