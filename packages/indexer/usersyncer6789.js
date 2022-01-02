0e0ad3c56756e9ec7d13eeb013b8b013d9a7d316
Austin Griffith <austin@ethereum.org>
2022-01-02 11:13:18 -0700

    5 once

--- packages/indexer/usersyncer6789.js
+++ packages/indexer/usersyncer6789.js
@@ -24,8 +24,7 @@ const main = async (s3) => {
 
     const addresses = fs.readdirSync("addresses")
     for(let a in addresses){
-    if(addresses[a].indexOf("0x5")===0
-      || addresses[a].indexOf("0x6")===0
+    if(addresses[a].indexOf("0x6")===0
       || addresses[a].indexOf("0x7")===0
       || addresses[a].indexOf("0x8")===0
       || addresses[a].indexOf("0x9")===0
