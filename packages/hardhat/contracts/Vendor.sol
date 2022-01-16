e7646240ab35345a08a5f0cf4973865b8c8eb3e5
Austin Griffith <austin@ethereum.org>
2022-01-16 14:15:36 -0700

    clean contract

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -17,23 +17,9 @@ contract Vendor is Ownable {
   uint256 public constant tokensPerEth = 100;
 
   // ToDo: create a payable buyTokens() function:
-  function buyTokens() public payable {
-    uint256 amount = msg.value * tokensPerEth;
-    yourToken.transfer(msg.sender,amount);
-    emit BuyTokens(msg.sender, msg.value, amount);
-  }
 
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
-  function withdraw() public onlyOwner {
-    (bool sent, ) = owner().call{value: address(this).balance}("");
-    require(sent, "failed to withdraw");
-  }
 
   // ToDo: create a sellTokens() function:
-  function sellTokens(uint256 amount) public {
-    yourToken.transferFrom(msg.sender, address(this), amount);
-    (bool sent, ) = msg.sender.call{value: amount/tokensPerEth}("");
-    require(sent, "failed to send ETH");
-  }
 
 }
