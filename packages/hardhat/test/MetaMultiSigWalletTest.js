2b233ba8a353953d44f6347aa9178df921b2c3d6
tamas.molnar <tamas.molnar@liferay.com>
2022-02-27 13:28:21 +0100

    Deploy MetaMultiSigWallet, set up some helper variables and write a simple isOwner test

--- packages/hardhat/test/MetaMultiSigWalletTest.js
+++ packages/hardhat/test/MetaMultiSigWalletTest.js
@@ -1,7 +1,30 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
 
-
 describe("MetaMultiSigWallet Test", () => {
+  let metaMultiSigWallet;
+  let owner;
+  let addr1;
+  let addr2;
+  let addr3;
+  let addrs;
 
+  const CHAIN_ID = 1; // I guess this number doesn't really matter
+  let signatureRequired = 1; // Starting with something straithforward
+
+  // Running this before each test
+  // Deploys MetaMultiSigWallet and sets up some addresses for easier testing
+  beforeEach(async function () {
+    [owner, addr1, addr2, addr3, ...addrs] = await ethers.getSigners();
+
+    let metaMultiSigWalletFactory = await ethers.getContractFactory("MetaMultiSigWallet");
+
+    metaMultiSigWallet = await metaMultiSigWalletFactory.deploy(CHAIN_ID, [owner.address], signatureRequired);
+  });
+
+  describe("Deployment", () => {
+    it("isOwner should return true for the owner address", async () => {     
+      expect(await metaMultiSigWallet.isOwner(owner.address)).to.equal(true);
+    });
+  });
 });
