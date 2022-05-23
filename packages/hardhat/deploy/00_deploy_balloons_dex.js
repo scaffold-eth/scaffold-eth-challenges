0c94231cc262e7811581ea32744c830ba1479d89
Ewan Humbert <xeway@protonmail.com>
2022-05-23 21:07:08 +0200

    Improved javascript syntax

--- packages/hardhat/deploy/00_deploy_balloons_dex.js
+++ packages/hardhat/deploy/00_deploy_balloons_dex.js
@@ -21,13 +21,13 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   // const dex = await ethers.getContract("DEX", deployer);
 
   // paste in your address here to get 10 balloons on deploy:
-  // await balloons.transfer("YOUR_ADDRESS",""+(10*10**18));
+  // await balloons.transfer("YOUR_ADDRESS", (10*10**18).toString());
 
   // uncomment to init DEX on deploy:
-  // console.log("Approving DEX ("+dex.address+") to take Balloons from main account...")
+  // console.log(`Approving DEX (${dex.address}) to take Balloons from main account...`)
   // If you are going to the testnet make sure your deployer account has enough ETH
-  // await balloons.approve(dex.address,ethers.utils.parseEther('100'));
+  // await balloons.approve(dex.address, ethers.utils.parseEther('100'));
   // console.log("INIT exchange...")
-  // await dex.init(""+(3*10**18),{value:ethers.utils.parseEther('3'),gasLimit:200000})
+  // await dex.init((3*10**18).toString(), {value:ethers.utils.parseEther('3'), gasLimit:200000})
 };
 module.exports.tags = ["Balloons", "DEX"];
