6f8ebb675d579c33c2aa0a81b1d342e91e6edb94
Zak G <zakgriffith@gmail.com>
2022-07-05 13:11:45 -0600

    Staker.sol Update comments

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -13,14 +13,14 @@ contract Staker {
   }
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
-  //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+  // ( Make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
 
 
   // After some `deadline` allow anyone to call an `execute()` function
-  //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
+  // If the deadline has passed and the threshold is met, it should call `exampleExternalContract.complete{value: address(this).balance}()` 
 
 
-  // if the `threshold` was not met, allow everyone to call a `withdraw()` function
+  // If the `threshold` was not met, allow everyone to call a `withdraw()` function
 
 
   // Add a `withdraw()` function to let users withdraw their balance
