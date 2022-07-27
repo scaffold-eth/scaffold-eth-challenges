aac16017415a1949a59e7bc6739c621866a4e14f
Zak G <zakgriffith@gmail.com>
2022-07-27 16:10:53 -0600

    challenge_2.js Updates
    Removed argument from calls to withdraw function to align with speed run instructions.

--- packages/hardhat/test/challenge_2.js
+++ packages/hardhat/test/challenge_2.js
@@ -154,7 +154,7 @@ describe("🚩 Challenge 2: 🏵 Token Vendor 🤖", function () {
       const startingNonOwnerETHBalance = await ethers.provider.getBalance(nonOwner.address)
       console.log('\t'," ⚖️  Starting non-owner ETH balance: ",ethers.utils.formatEther(startingNonOwnerETHBalance))
 
-      await expect(vendor.connect(nonOwner).withdraw(vendorETHBalance)).to.be.revertedWith("Ownable: caller is not the owner");
+      await expect(vendor.connect(nonOwner).withdraw()).to.be.revertedWith("Ownable: caller is not the owner");
       console.log('\t'," 🏷  withdraw failed with correct error");
 
       const newNonOwnerETHBalance = await ethers.provider.getBalance(nonOwner.address)
@@ -164,7 +164,7 @@ describe("🚩 Challenge 2: 🏵 Token Vendor 🤖", function () {
       console.log('\t'," 🍾 Withdrawing as owner...")
       const startingOwnerETHBalance = await ethers.provider.getBalance(owner.address)
       console.log('\t'," ⚖️  Starting owner ETH balance: ",ethers.utils.formatEther(startingOwnerETHBalance))
-      const withdrawResult = await vendor.withdraw(vendorETHBalance);
+      const withdrawResult = await vendor.withdraw();
       console.log('\t'," 🏷  withdraw Result: ",withdrawResult.hash);
 
       console.log('\t'," ⏳ Waiting for confirmation...")
@@ -182,135 +182,4 @@ describe("🚩 Challenge 2: 🏵 Token Vendor 🤖", function () {
 
     });
   })
-
-
-
-
-
-  //console.log("hre:",Object.keys(hre)) // <-- you can access the hardhat runtime env here
-  /*
-  describe("Staker", function () {
-
-    if(process.env.CONTRACT_ADDRESS){
-      it("Should connect to external contract", async function () {
-        stakerContract = await ethers.getContractAt("Staker",process.env.CONTRACT_ADDRESS);
-        console.log("     🛰 Connected to external contract",myContract.address)
-      });
-    }else{
-      it("Should deploy ExampleExternalContract", async function () {
-        const ExampleExternalContract = await ethers.getContractFactory("ExampleExternalContract");
-        exampleExternalContract = await ExampleExternalContract.deploy();
-      });
-      it("Should deploy Staker", async function () {
-        const Staker = await ethers.getContractFactory("Staker");
-        stakerContract = await Staker.deploy(exampleExternalContract.address);
-      });
-    }
-
-    describe("mintItem()", function () {
-      it("Balance should go up when you stake()", async function () {
-        const [ owner ] = await ethers.getSigners();
-
-        console.log('\t'," 🧑‍🏫 Tester Address: ",owner.address)
-
-        const startingBalance = await stakerContract.balances(owner.address)
-        console.log('\t'," ⚖️ Starting balance: ",startingBalance.toNumber())
-
-        console.log('\t'," 🔨 Staking...")
-        const stakeResult = await stakerContract.stake({value: ethers.utils.parseEther("0.001")});
-        console.log('\t'," 🏷  stakeResult: ",stakeResult.hash)
-
-        console.log('\t'," ⏳ Waiting for confirmation...")
-        const txResult =  await stakeResult.wait()
-        expect(txResult.status).to.equal(1);
-
-        const newBalance = await stakerContract.balances(owner.address)
-        console.log('\t'," 🔎 New balance: ", ethers.utils.formatEther(newBalance))
-        expect(newBalance).to.equal(startingBalance.add(ethers.utils.parseEther("0.001")));
-
-      });
-
-
-      if(process.env.CONTRACT_ADDRESS){
-        console.log(" 🤷 since we will run this test on a live contract this is as far as the automated tests will go...")
-      }else{
-
-        it("If enough is staked and time has passed, you should be able to complete", async function () {
-
-          const timeLeft1 = await stakerContract.timeLeft()
-          console.log('\t',"⏱ There should be some time left: ",timeLeft1.toNumber())
-          expect(timeLeft1.toNumber()).to.greaterThan(0);
-
-
-          console.log('\t'," 🚀 Staking a full eth!")
-          const stakeResult = await stakerContract.stake({value: ethers.utils.parseEther("1")});
-          console.log('\t'," 🏷  stakeResult: ",stakeResult.hash)
-
-          console.log('\t'," ⌛️ fast forward time...")
-          await network.provider.send("evm_increaseTime", [3600])
-          await network.provider.send("evm_mine")
-
-          const timeLeft2 = await stakerContract.timeLeft()
-          console.log('\t',"⏱ Time should be up now: ",timeLeft2.toNumber())
-          expect(timeLeft2.toNumber()).to.equal(0);
-
-          console.log('\t'," 🎉 calling execute")
-          const execResult = await stakerContract.execute();
-          console.log('\t'," 🏷  execResult: ",execResult.hash)
-
-          const result = await exampleExternalContract.completed()
-          console.log('\t'," 🥁 complete: ",result)
-          expect(result).to.equal(true);
-
-        })
-      }
-
-
-      it("Should redeploy Staker, stake, not get enough, and withdraw", async function () {
-        const [ owner, secondAccount ] = await ethers.getSigners();
-
-        const ExampleExternalContract = await ethers.getContractFactory("ExampleExternalContract");
-        exampleExternalContract = await ExampleExternalContract.deploy();
-
-        const Staker = await ethers.getContractFactory("Staker");
-        stakerContract = await Staker.deploy(exampleExternalContract.address);
-
-        console.log('\t'," 🔨 Staking...")
-        const stakeResult = await stakerContract.stake({value: ethers.utils.parseEther("0.001")});
-        console.log('\t'," 🏷  stakeResult: ",stakeResult.hash)
-
-        console.log('\t'," ⏳ Waiting for confirmation...")
-        const txResult =  await stakeResult.wait()
-        expect(txResult.status).to.equal(1);
-
-        console.log('\t'," ⌛️ fast forward time...")
-        await network.provider.send("evm_increaseTime", [3600])
-        await network.provider.send("evm_mine")
-
-        console.log('\t'," 🎉 calling execute")
-        const execResult = await stakerContract.execute();
-        console.log('\t'," 🏷  execResult: ",execResult.hash)
-
-        const result = await exampleExternalContract.completed()
-        console.log('\t'," 🥁 complete should be false: ",result)
-        expect(result).to.equal(false);
-
-
-        const startingBalance = await ethers.provider.getBalance(secondAccount.address);
-        //console.log("startingBalance before withdraw", ethers.utils.formatEther(startingBalance))
-
-        console.log('\t'," 💵 calling withdraw")
-        const withdrawResult = await stakerContract.withdraw(secondAccount.address);
-        console.log('\t'," 🏷  withdrawResult: ",withdrawResult.hash)
-
-        const endingBalance = await ethers.provider.getBalance(secondAccount.address);
-        //console.log("endingBalance after withdraw", ethers.utils.formatEther(endingBalance))
-
-        expect(endingBalance).to.equal(startingBalance.add(ethers.utils.parseEther("0.001")));
-
-
-      });
-
-    });
-  });*/
 });
