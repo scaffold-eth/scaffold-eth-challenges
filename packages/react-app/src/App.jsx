28bb66f067bd6dfbbfd1a092d0ab5e47549b1080
Zak G <zakgriffith@gmail.com>
2022-08-11 10:51:08 -0600

    Challenge 0 JS - App.jsx to Include Infura IPFS Info

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -29,9 +29,18 @@ import { Transactor } from "./helpers";
 import { useContractConfig } from "./hooks";
 // import Hints from "./Hints";
 
+const projectId = "2DDHiA47zFkJXtnxzl2jFkyuaoq";
+const projectSecret = "96a91eeafc0a390ab66e6a87f61152aa";
+const projectIdAndSecret = `${projectId}:${projectSecret}`;
+
 const { BufferList } = require("bl");
 const ipfsAPI = require("ipfs-http-client");
-const ipfs = ipfsAPI({ host: "ipfs.infura.io", port: "5001", protocol: "https" });
+const ipfs = ipfsAPI({
+  host: "ipfs.infura.io",
+  port: "5001",
+  protocol: "https",
+  headers: { authorization: `Basic ${Buffer.from(projectIdAndSecret).toString("base64")}` },
+});
 
 const { ethers } = require("ethers");
 
