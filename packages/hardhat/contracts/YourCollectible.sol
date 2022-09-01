97a8347f2b10f556c462568bce298de14c3fc30d
Zak G <zakgriffith@gmail.com>
2022-09-01 11:52:55 -0600

    YourCollectable.sol - Add comment about solidity version

--- packages/hardhat/contracts/YourCollectible.sol
+++ packages/hardhat/contracts/YourCollectible.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
+pragma solidity ^0.8.2;  //Do not change the solidity version as it negativly impacts submission grading
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
