d89bcf72a2e230acded2f97cb306b059a1774ef4
Zak G <zakgriffith@gmail.com>
2022-09-01 12:14:15 -0600

    Add comments about solidity version

--- packages/hardhat/contracts/RiggedRoll.sol
+++ packages/hardhat/contracts/RiggedRoll.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity >=0.8.0 <0.9.0;  //Do not change the solidity version as it negativly impacts submission grading
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
