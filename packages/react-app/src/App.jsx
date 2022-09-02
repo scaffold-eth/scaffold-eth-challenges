3bea7f96c052fa1f96afec13ff8023118a361d34
Zak G <zakgriffith@gmail.com>
2022-09-02 12:42:35 -0600

    App.jsx.  Short term - Use Infura until pinata

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -29,27 +29,29 @@ import { Transactor } from "./helpers";
 import { useContractConfig } from "./hooks";
 // import Hints from "./Hints";
 
-//const projectId = "**Your Infura Project ID**";
-//const projectSecret = "**Your Infura Project Secret**";;
-//const projectIdAndSecret = `${projectId}:${projectSecret}`;
+const projectId = "2DDHiA47zFkJXtnxzl2jFkyuaoq";
+const projectSecret = "96a91eeafc0a390ab66e6a87f61152aa";;
+const projectIdAndSecret = `${projectId}:${projectSecret}`;
 
 const { BufferList } = require("bl");
 const ipfsAPI = require("ipfs-http-client");
 
+/*
 const ipfs = ipfsAPI({
   host: "ipfs.nifty.ink",
   port: "3001",
   protocol: "https",
 });
+*/
 
-/*
+
 const ipfs = ipfsAPI({
   host: "ipfs.infura.io",
   port: "5001",
   protocol: "https",
   headers: { authorization: `Basic ${Buffer.from(projectIdAndSecret).toString("base64")}` },
 });
-*/
+
 
 const { ethers } = require("ethers");
 
