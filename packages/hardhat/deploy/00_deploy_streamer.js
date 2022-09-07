535253625657fe52a6fcdaa5ee07bd9c3bb933ed
Colin Kennedy <colin.kennedy@mesh.xyz>
2022-09-06 22:20:45 -0300

    tag checkpoint 2 entry, remove unused deploy code

--- packages/hardhat/deploy/00_deploy_streamer.js
+++ packages/hardhat/deploy/00_deploy_streamer.js
@@ -2,8 +2,6 @@
 
 const { ethers } = require("hardhat");
 
-// const { ethers } = require("hardhat");
-
 module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
@@ -16,22 +14,15 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
 
   const streamer = await ethers.getContract("Streamer", deployer);
 
-  // ToDo: change address to your frontend address vvvv
   console.log("\n 🤹  Sending ownership to frontend address...\n");
-  const ownershipTransaction = await streamer.transferOwnership(
-    "0xdb0dFc84F0D18A29254aBe01debfb3dC69A1632c"
-  );
+  // Checkpoint 2: change address to your frontend address vvvv
+  const ownerTx = await streamer.transferOwnership("your_address_here");
+
   console.log("\n       confirming...\n");
-  const ownershipResult = await ownershipTransaction.wait();
+  const ownershipResult = await ownerTx.wait();
   if (ownershipResult) {
     console.log("       ✅ ownership transferred successfully!\n");
   }
-
-  // If you want to link a library into your contract:
-  // reference: https://github.com/austintgriffith/scaffold-eth/blob/using-libraries-example/packages/hardhat/scripts/deploy.js#L19
-  // const yourContract = await deploy("YourContract", [], {}, {
-  //  LibraryName: **LibraryAddress**
-  // });
 };
 
 module.exports.tags = ["Streamer"];
