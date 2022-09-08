59f7f2bdb3d2b4ef189e8fb52e928f3410414cb8
Kent Miguel <kentmiguel10@gmail.com>
2022-09-08 10:51:07 +0200

    Update YourToken.sol
    checkpoints 1 and 2

--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -1,11 +1,12 @@
-pragma solidity 0.8.4;  //Do not change the solidity version as it negativly impacts submission grading
+pragma solidity 0.8.4; //Do not change the solidity version as it negativly impacts submission grading
 // SPDX-License-Identifier: MIT
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 // learn more: https://docs.openzeppelin.com/contracts/4.x/erc20
 
 contract YourToken is ERC20 {
     constructor() ERC20("Gold", "GLD") {
-        //_mint( ~~~YOUR FRONTEND ADDRESS HERE~~~~ , 1000 * 10 ** 18);
+        _mint(0xc16FFADf86FA0071eaeD7589FfeE56e88a076a88, 1000 * 10**18);
     }
 }
