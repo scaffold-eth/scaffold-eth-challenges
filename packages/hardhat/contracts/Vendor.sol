fcdb9de67095422d95bfc3f55d55510ef17f32fd
Kent Miguel <kentmiguel10@gmail.com>
2022-09-08 09:32:10 +0200

    Update Vendor.sol

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,23 +1,23 @@
-pragma solidity 0.8.4;  //Do not change the solidity version as it negativly impacts submission grading
+pragma solidity 0.8.4; //Do not change the solidity version as it negativly impacts submission grading
 // SPDX-License-Identifier: MIT
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
 contract Vendor is Ownable {
+    //Test
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
-  YourToken public yourToken;
+    YourToken public yourToken;
 
-  constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
-  }
+    constructor(address tokenAddress) {
+        yourToken = YourToken(tokenAddress);
+    }
 
-  // ToDo: create a payable buyTokens() function:
+    // ToDo: create a payable buyTokens() function:
 
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
+    // ToDo: create a withdraw() function that lets the owner withdraw ETH
 
-  // ToDo: create a sellTokens(uint256 _amount) function:
-
+    // ToDo: create a sellTokens(uint256 _amount) function:
 }
