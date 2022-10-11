6f55e553119a65f18ad06c1460f4df31e1459525
Zak Griffith <zakgriffith@gmail.com>
2022-10-11 16:24:13 -0600

    Commenting ownership transfer
    This is so the initial yarn deploy doesn't error out.

--- packages/hardhat/deploy/00_deploy_streamer.js
+++ packages/hardhat/deploy/00_deploy_streamer.js
@@ -14,15 +14,15 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
 
   const streamer = await ethers.getContract("Streamer", deployer);
 
-  console.log("\n 🤹  Sending ownership to frontend address...\n");
-  // Checkpoint 2: change address to your frontend address vvvv
-  const ownerTx = await streamer.transferOwnership("your_address_here");
+  //console.log("\n 🤹  Sending ownership to frontend address...\n");
+  //Checkpoint 2: change address to your frontend address vvvv
+  //const ownerTx = await streamer.transferOwnership("your_address_here");
 
-  console.log("\n       confirming...\n");
-  const ownershipResult = await ownerTx.wait();
-  if (ownershipResult) {
-    console.log("       ✅ ownership transferred successfully!\n");
-  }
+  // console.log("\n       confirming...\n");
+  // const ownershipResult = await ownerTx.wait();
+  // if (ownershipResult) {
+  //   console.log("       ✅ ownership transferred successfully!\n");
+  // }
 };
 
 module.exports.tags = ["Streamer"];
