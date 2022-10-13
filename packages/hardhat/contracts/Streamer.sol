2c9b6b6f50e04bbe13f401f59368a1f23f497242
Zak Griffith <zakgriffith@gmail.com>
2022-10-13 12:27:43 -0600

    Streamer.sol Comments
    Add comments to emit Closed and Withdrawn events.   Some spacing fixes.

--- packages/hardhat/contracts/Streamer.sol
+++ packages/hardhat/contracts/Streamer.sol
@@ -7,8 +7,8 @@ import "hardhat/console.sol";
 contract Streamer is Ownable {
     event Opened(address, uint256);
     event Challenged(address);
-    event Closed(address);
     event Withdrawn(address, uint256);
+    event Closed(address);
 
     mapping(address => uint256) balances;
     mapping(address => uint256) canCloseAt;
@@ -53,16 +53,12 @@ contract Streamer is Ownable {
       Checkpoint 5: Recover earnings
 
       The service provider would like to cash out their hard earned ether.
-    */
-
-        // use ecrecover on prefixedHashed and the supplied signature
-
-        // require that the recovered signer has a running channel with balances[signer] > v.updatedBalance
-
-        // calculate the payment when reducing balances[signer] to v.updatedBalance
-
-        // adjust the channel balance, and pay the contract owner. (Get the owner address with
-        // the `owner()` function)
+          - use ecrecover on prefixedHashed and the supplied signature
+          - require that the recovered signer has a running channel with balances[signer] > v.updatedBalance
+          - calculate the payment when reducing balances[signer] to v.updatedBalance
+          - adjust the channel balance, and pay the contract owner. (Get the owner address withthe `owner()` function)
+          - emit the Withdrawn event
+      */
     }
 
     /*
@@ -72,7 +68,7 @@ contract Streamer is Ownable {
     - checks that msg.sender has an open channel
     - updates canCloseAt[msg.sender] to some future time
     - emits a Challenged event
-  */
+    */
 
     /*
     Checkpoint 6b: Close the channel
@@ -80,9 +76,9 @@ contract Streamer is Ownable {
     create a public defundChannel() function that:
     - checks that msg.sender has a closing channel
     - checks that the current time is later than the closing time
-    - sends the channel's remaining funds to msg.sender, and sets
-      the balance to 0
-  */
+    - sends the channel's remaining funds to msg.sender, and sets the balance to 0
+    - emits the Closed event
+    */
 
     struct Voucher {
         uint256 updatedBalance;
