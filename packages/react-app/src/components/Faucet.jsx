53b8fe741a9ac8f1706e78402a5cece706c9918e
Zak Griffith <zakgriffith@gmail.com>
2022-10-13 16:31:33 -0600

    Component updates to most recent

--- packages/react-app/src/components/Faucet.jsx
+++ packages/react-app/src/components/Faucet.jsx
@@ -1,7 +1,8 @@
-import { Button, Input, Tooltip } from "antd";
-import React, { useState, useEffect } from "react";
-import Blockies from "react-blockies";
 import { SendOutlined } from "@ant-design/icons";
+import { Button, Input, Tooltip } from "antd";
+// import { useLookupAddress } from "eth-hooks/dapps/ens";
+import React, { useCallback, useState, useEffect } from "react";
+import Blockies from "react-blockies";
 import { Transactor } from "../helpers";
 import Wallet from "./Wallet";
 
@@ -11,7 +12,7 @@ const { utils } = require("ethers");
 // added option to directly input ens name
 // added placeholder option
 
-/**
+/*
   ~ What it does? ~
 
   Displays a local faucet to send ETH to given address, also wallet is provided
@@ -33,19 +34,20 @@ const { utils } = require("ethers");
               (ex. "0xa870" => "user.eth") or you can enter directly ENS name instead of address
               works both in input field & wallet
   - Provide placeholder="Send local faucet" value for the input
-**/
+*/
 
 export default function Faucet(props) {
   const [address, setAddress] = useState();
   const [faucetAddress, setFaucetAddress] = useState();
 
-  const { price, placeholder, localProvider, ensProvider } = props;
+  const { price, placeholder, localProvider, ensProvider, onChange } = props;
 
   useEffect(() => {
     const getFaucetAddress = async () => {
       if (localProvider) {
         const _faucetAddress = await localProvider.listAccounts();
         setFaucetAddress(_faucetAddress[0]);
+        //console.log(_faucetAddress);
       }
     };
     getFaucetAddress();
@@ -58,11 +60,26 @@ export default function Faucet(props) {
     blockie = <div />;
   }
 
-  const updateAddress = newValue => {
-    if (typeof newValue !== "undefined" && utils.isAddress(newValue)) {
-      setAddress(newValue);
-    }
-  };
+  // const ens = useLookupAddress(ensProvider, address);
+
+  const updateAddress = useCallback(
+    async newValue => {
+      if (typeof newValue !== "undefined" && utils.isAddress(newValue)) {
+        let newAddress = newValue;
+        // if (newAddress.indexOf(".eth") > 0 || newAddress.indexOf(".xyz") > 0) {
+        //   try {
+        //     const possibleAddress = await ensProvider.resolveName(newAddress);
+        //     if (possibleAddress) {
+        //       newAddress = possibleAddress;
+        //     }
+        //     // eslint-disable-next-line no-empty
+        //   } catch (e) { }
+        // }
+        setAddress(newAddress);
+      }
+    },
+    [ensProvider, onChange],
+  );
 
   const tx = Transactor(localProvider);
 
@@ -73,14 +90,18 @@ export default function Faucet(props) {
         placeholder={placeholder ? placeholder : "local faucet"}
         prefix={blockie}
         value={address}
-        onChange={e => updateAddress(e.target.value)}
+        // value={ens || address}
+        onChange={e => {
+          // setAddress(e.target.value);
+          updateAddress(e.target.value);
+        }}
         suffix={
           <Tooltip title="Faucet: Send local ether to an address.">
             <Button
               onClick={() => {
                 tx({
                   to: address,
-                  value: utils.parseEther("1.5"),
+                  value: utils.parseEther("0.01"),
                 });
                 setAddress("");
               }}
