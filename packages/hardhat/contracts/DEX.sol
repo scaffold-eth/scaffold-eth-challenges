8d6f24e477d7f2bdf15563bbf1e2eca127535212
Ben Dumoulin <31484050+ben132333@users.noreply.github.com>
2023-03-10 07:20:55 -0500

    remove deprecated use of SafeMath
    See: 
    - https://docs.soliditylang.org/en/v0.8.9/080-breaking-changes.html
    - https://github.com/OpenZeppelin/openzeppelin-contracts/commit/6f23efa97056e643cefceedf86fdf1206b6840fb#diff-f4b1737177aad965d94530b54ac4001a2e1f5fe6e4e34bafe023310cea599eca

--- packages/hardhat/contracts/DEX.sol
+++ packages/hardhat/contracts/DEX.sol
@@ -3,7 +3,6 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 /**
  * @title DEX Template
@@ -15,7 +14,6 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 contract DEX {
     /* ========== GLOBAL VARIABLES ========== */
 
-    using SafeMath for uint256; //outlines use of SafeMath for uint256 variables
     IERC20 token; //instantiates the imported contract
 
     /* ========== EVENTS ========== */
